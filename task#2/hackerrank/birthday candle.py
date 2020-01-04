7685f6386a298e90e6b633d140069d15f0e768e4
Navneet singh <53183532+codenavneet@users.noreply.github.com>
2020-01-05 02:20:33 +0530

    task#2 PART A

---
+++ task#2/hackerrank/birthday candle.py
@@ -0,0 +1,7 @@
+n = int(input())# takes input of age of girl
+
+l= input().split()#height of candles
+
+t= l.count(max(l))#no of candles of max height in l
+
+print(t)#total no of candles she is able to blow
