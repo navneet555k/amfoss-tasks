5da91df4280f0c5ed5cbcf2fb244a04e797bc6be
Navneet singh <53183532+codenavneet@users.noreply.github.com>
2020-01-27 01:51:43 +0530

    Delete next round.py

--- task #2/codeforces/next round.py
+++
@@ -1,8 +0,0 @@
-l=input().split()
-for i in range(len(l[0])):
-    points=input().split()
-count=0
-for i in range(int(l[0])):
-    if int(points[i])>=int(points[int(l[1])-1]) :
-        count=count+1    
-print (count)
