f3024ab1dbe41d1bb12588d0886bd511aab779ca
Navneet singh <53183532+codenavneet@users.noreply.github.com>
2020-01-27 01:53:26 +0530

    Delete way to long words.py

--- task #2/codeforces/way to long words.py
+++
@@ -1,8 +0,0 @@
-n=int(input())
-for i in range(0,n):
-          word=input("")
-          if len(word)>10:
-                    
-            print(word[0],len(word)-2,word[-1])
-          else:
-            print(word)
